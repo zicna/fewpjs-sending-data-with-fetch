15af51df6994d0c92a21556bce395b4c53c5cb0b
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-01 11:03:54 -0500

    commit tree 7caee2d08e83180ac8f3b4fc09d569ae48eff3cd

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Add your code here
+// const formData = {
+//   dogName: "Byron",
+//   dogBreed: "Poodle",
+// };
+
+// const configurationObject = {
+//     method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "applicatio/json",
+//   },
+//   body: JSON.stringify(formData),
+// };
+// const baseUrl = "http://localhost:3000/dogs";
+
+// fetch(baseUrl, configurationObject)
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     alert("Bad things! Ragnarok!");
+//     console.log(error.message);
+//   });
+
+function submitData(userName, userEmail) {
+  const baseUrl = "http://localhost:3000/users";
+  const configObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail,
+    }),
+  };
+  return fetch(baseUrl, configObject)
+    .then((response) => response.json())
+    .then((data) => appendToDOM(data.id)).catch(error => appendToDOM(error))
+}
+
+function appendToDOM(data) {
+  let body = document.getElementsByTagName("body")[0];
+  body.innerHTML = data;
+}
+
+submitData("Milan", "milan@zicna.com");
